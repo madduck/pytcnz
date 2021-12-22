6dd05ab61a88ef6b35a8d8c1aa744440ba2a0b01
martin f. krafft <madduck@madduck.net>
2021-12-22 21:43:14 +1300

    Disable Flake8 tests when selecting specific tests

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,11 @@ if command -v ipython3 >/dev/null; then
   ipdb='--pdbcls=IPython.terminal.debugger:TerminalPdb'
 fi
 
-exec pytest-3 $ipdb --color=no -rfExP --tb=native --showlocals --flake8 "$@"
+# --flake8 does not work if individual tests are specified, i.e. by passing
+# e.g. tests/test_squashnz_player.py::test_vaccination_status_expired
+case "$@" in
+  (*.py::test*) flake=;;
+  (*) flake=--flake8;;
+esac
+
+exec pytest-3 $ipdb --color=no -rfExP --tb=native --showlocals $flake "$@"
