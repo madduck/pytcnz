77146cb9bcb4ae8967adfa4cc133d6e762950fe3
martin f. krafft <madduck@madduck.net>
2022-04-26 20:53:24 +1200

    Nicer game labels for rounds 1&2

--- pytcnz/squashnz/game_names.py
+++ pytcnz/squashnz/game_names.py
@@ -6,10 +6,21 @@
 
 GAME_NAMES_BY_DRAW_SIZE = {
     6: {
+        "101": ("#1 vs. #6", "1 X 6"),
+        "102": ("#2 vs. #5", "2 X 5"),
+        "103": ("#3 vs. #4", "3 X 4"),
         "401": ("Championship Final", "Final"),
         "402": ("Plate Final", "Plate"),
     },
     8: {
+        "101": ("#1 vs. #8", "1 X 8"),
+        "102": ("#4 vs. #5", "4 X 5"),
+        "103": ("#3 vs. #6", "3 X 6"),
+        "104": ("#2 vs. #7", "2 X 7"),
+        "201": ("Semi Final A", "Semi A"),
+        "202": ("Semi Final B", "Semi B"),
+        "203": ("Plate Semi A", "Pl.Semi A"),
+        "204": ("Plate Semi B", "Pl.Semi B"),
         "301": ("Championship Final", "Final"),
         "302": ("Special Plate Final", "Sp.Plate"),
         "303": ("Plate Final", "Plate"),
